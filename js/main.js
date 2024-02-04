bcb8477c773dc8ecac91a247b73fb739c65f96f1
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-04 01:35:46 +0100

    added remote control volume function and mute functions

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
 //Primero obtenemos todos los elementos de la clase cuadrado u otra en un htmlcollection
-
+const VOLUMEN_TEXT_MSG_TIME = 2500
 let screen = document.getElementById("screen") // me trae el div con id frame
 
-console.log(screen)
-
-// function change() {
-//   let div1 = document.getElementById("divUno")
-//   let div2 = document.getElementsByClassName("divDos")
-//   if (div1.hidden == true && div2[0].hidden == false) {
-//     div1.hidden = false
-//     div2[0].hidden = true
-//   } else {
-//     div1.hidden = true
-//     div2[0].hidden = false
-//   }
-// }
-// function addHtml() {
-//   let escrito = document.getElementById("input1")
-//   let objetoDiv = document.getElementById("contenedor")
-//   let p = document.createElement("p")
-//   p.innerHTML = escrito.value
-//   console.log(p)
-//   console.log(p.innerHTML)
-//   escrito.value = ""
-//   objetoDiv.appendChild(p)
-// }
+function change() {
+  let div1 = document.getElementById("divUno")
+  let div2 = document.getElementsByClassName("divDos")
+  if (div1.hidden == true && div2[0].hidden == false) {
+    div1.hidden = false
+    div2[0].hidden = true
+  } else {
+    div1.hidden = true
+    div2[0].hidden = false
+  }
+}
+function addHtml() {
+  let escrito = document.getElementById("input1")
+  let objetoDiv = document.getElementById("contenedor")
+  let p = document.createElement("p")
+  p.innerHTML = escrito.value
+  console.log(p)
+  console.log(p.innerHTML)
+  escrito.value = ""
+  objetoDiv.appendChild(p)
+}
 
 setInterval(hora, 1000)
 
@@ -90,3 +88,65 @@ function buttonNumbers() {
     })
   })
 }
+let contVolume = 3
+let v
+volume()
+const volumeIcon = document.getElementById("volumeIcon")
+function setVolumeIcon() {
+  contVolume !== 0
+    ? ((volumeIcon.innerHTML = v),
+      setTimeout(() => {
+        volumeIcon.innerHTML = ""
+      }, VOLUMEN_TEXT_MSG_TIME))
+    : (mute(), (toggleMute = true))
+}
+function volume() {
+  const volumeButton = document.getElementsByClassName("volume")
+
+  volumeButton[0].addEventListener("click", (evento) => {
+    console.log(evento.target.innerHTML)
+    // let volumeIcon = document.getElementById("volumeIcon")
+    if (evento.target.innerHTML === "V +") {
+      //Sube el volumen
+      contVolume++
+      v = ` 🔊⬆ ${contVolume}`
+      toggleMute = false
+    } else {
+      // Baja el volumen
+      if (contVolume > 0) {
+        contVolume--
+        v = `🔊⬇ ${contVolume}`
+      } else {
+        // v = "silencio 🔇 "
+        setVolumeIcon()
+        return
+      }
+    }
+    console.log(volumeIcon)
+    setVolumeIcon()
+  })
+}
+// inicializo bandera volumen y memoria volumen
+let toggleMute = false
+let memoryVolume
+// Funcion Mute pone el volumen = 0
+function mute() {
+  memoryVolume = contVolume
+  contVolume = 0
+  volumeIcon.innerHTML = "silencio 🔇 "
+}
+const muteButton = document.getElementById("mute")
+muteButton.addEventListener("click", () => {
+  toggleMute = !toggleMute
+  // pregunto por estado de bandera mute para silenciar o no
+  toggleMute
+    ? (mute(), console.log("mute on"))
+    : (console.log("mute off"),
+      //si el volumen era 0, al sacar mute se pone en 1
+      //si el volumen !=0, al sacar volumen vuelve al valor anterior
+      !memoryVolume
+        ? (contVolume = memoryVolume + 1)
+        : (contVolume = memoryVolume),
+      (v = ` 🔊 ${contVolume}`),
+      setVolumeIcon())
+})
