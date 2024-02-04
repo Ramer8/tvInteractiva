7be493df3a4f753bc14f1c734aa7de91505da429
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-05 00:45:41 +0100

    readme file created and comments deleted

---
+++ README.md
@@ -0,0 +1,125 @@
+# SMART TV APP
+
+This is the second Proyect of the Full Stack Development Bootcamp with Geekshubs Academy. Based only on HTML, CSS and JAVASCRIPT.
+
+<details>
+  <summary>Content 📝</summary>
+  <ol>
+    <li><a href="#objetive-🎯 ">Objetive</a></li>
+    <li><a href="#about-a-proyect-🔎-🔎">About to the proyect</a></li>
+    <li><a href="#deploy-🚀">Deploy</a></li>
+    <li><a href="#stack">Stack</a></li>
+    <li><a href="#local-installation">Installation</a></li>
+    <li><a href="#view">View</a></li>
+    <li><a href="#contributions">Contributions</a></li>
+    <li><a href="#license">Licence</a></li>
+    <li><a href="#webgraphy">Webgraphy</a></li>
+    <li><a href="#development">Development</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+## Objetive 🎯
+
+This proyect it's development in HTML, don't need any configuration.
+
+## About a proyect 🔎
+
+It is a simple web page about thats simulated a smart tv whit his remote control.
+
+## Deploy 🚀
+
+<div align="center">
+    <a href="https://ramer8.github.io/tvInteractiva/"><strong>Url a producción </strong></a>🚀🚀🚀
+</div>
+
+## Stack
+
+<div align="center">
+
+<div align="center">
+<a href="https://developer.mozilla.org/es/docs/Web/HTML">
+    <img src= "https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white"/>
+</a>
+<a href="https://developer.mozilla.org/es/docs/Web/CSS">
+    <img src= "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"/>
+</a>
+<a href="https://www.github.com/">
+    <img src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"/>
+</a>
+
+<a href="https://www.github.com/">
+    <img src= "https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"/>
+</a>
+ </div>
+
+## Local installation
+
+1. Clone the repository
+2. Install live server extension in Visual Studio Code
+3. From index.html go live to see the web page.
+
+## View
+
+Landing/Home
+<img src="./img/view/landing.png">  
+Main Page
+<img src="./img/view/menu.png">
+Contact
+<img src="./img/view/contact.png">
+Landing/Home Mobile
+<img src="./img/view/landingMobile.png">
+Menu Mobile
+<img src="./img/view/menuMobile.png">
+Contact Mobile
+<img src="./img/view/contactMobile.png">
+
+## Contributions
+
+Suggestions and contributions are always welcome.
+
+You can do it in two ways:
+
+1.Opening an issue
+2.Create a fork of the repository
+
+- Createa a new branch
+  ```
+  $ git checkout -b feature/userName-improvement
+  ```
+- Make a commit with your changes
+  ```
+  $ git commit -m 'feat: better thing by'
+  ```
+- Push the branch
+  ```
+  $ git push origin feature/userName-improvement
+  ```
+- Open a Pull Request
+
+## License
+
+This project is under license by Ramiro Poblete
+
+## Webgraphy:
+
+To achieve my goal I have collected information from:
+
+- https://developer.mozilla.org/es/
+
+## Development:
+
+```js
+const developer = "Ramiro Poblete"
+
+console.log("Developed by: Ramiro Poblete + GeekHub")
+```
+
+## Contact
+
+<a href = "mailto:ramirolpoblete@gmail.com"><img src="https://img.shields.io/badge/Gmail-C6362C?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
+<a href="https://www.linkedin.com/in/ramiropoblete/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a>
+<a href = "https://github.com/Ramer8"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" target="_blank"></a>
+
+</p>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 //Primero obtenemos todos los elementos de la clase cuadrado u otra en un htmlcollection
-const VOLUMEN_TEXT_MSG_TIME = 2500
+const TEXT_MSG_TIME = 2500
 let screen = document.getElementById("screen") // me trae el div con id frame
 
+const screenHeader = document.getElementById("screenHeader")
+
+const power = document.getElementById("power")
+
+power.addEventListener("click", (evento) => {
+  console.log(screen.classList)
+  if (powerOn) {
+    console.log("encendido")
+    screen.classList = []
+    screen.classList.add("channel0")
+  }
+  if (!powerOn) {
+    screen.classList = []
+    screen.classList.add("powerOff")
+  }
+})
+// inicialzo bandera encendido/apagado
+let powerOn = false
+
+function toggle() {
+  let screenHeader = document.getElementById("screenHeader")
+  powerOn = !powerOn
+  screenHeader.hidden = !screenHeader.hidden
+  console.log(powerOn)
+}
+if (screenHeader.hidden) {
+  console.log("apagado")
+} else {
+  console.log("encendido")
+}
+
+// Hora y fecha
 setInterval(hora, 1000)
 
 function hora() {
@@ -16,56 +48,22 @@ function hora() {
   document.getElementById("fecha").innerHTML = date
 }
 
-const screenHeader = document.getElementById("screenHeader")
-
-const power = document.getElementById("power")
-
-power.addEventListener("click", (evento) => {
-  console.log(screen.classList)
-  console.log(screen.classList.length)
-  if (powerOn) {
-    console.log("encendido")
-    screen.classList = []
-    screen.classList.add("channel0")
-  }
-  if (!powerOn) {
-    screen.classList = []
-    screen.classList.add("powerOff")
-  }
-})
-// inicialzo bandera encendido/apagado
-let powerOn = false
-function toggle() {
-  let screenHeader = document.getElementById("screenHeader")
-  powerOn = !powerOn
-  screenHeader.hidden = !screenHeader.hidden
-  console.log(powerOn)
-}
-
-if (screenHeader.hidden) {
-  console.log("apagado")
-} else {
-  console.log("encendido")
-}
-
+// Responde a los numeros del control remoto
 buttonNumbers()
 
 function buttonNumbers() {
   const numbers = document.getElementsByClassName("numbers")
 
   let arrayNumbers = Array.from(numbers)
-  //   console.log(arrayNumbers)
-  screen = document.getElementById("screen") // me trae el div con id frame
+  screen = document.getElementById("screen") // me trae el div con id screen
 
   arrayNumbers.map((item) => {
     item.addEventListener("click", (evento) => {
-      console.log("presionaste un nro")
       screen.classList.remove(screen.classList[screen.classList.length - 1])
-      console.log(screen.classList)
-      console.log(evento.target.innerHTML)
       screen.classList.add("channel" + evento.target.innerHTML)
-
-      console.log(screen.classList)
+      console.log("channel " + evento.target.innerHTML)
+      document.getElementById("channelInfo").innerHTML =
+        "channel " + evento.target.innerHTML
     })
   })
 }
@@ -73,14 +71,19 @@ let contVolume = 3
 let v
 volume()
 const volumeIcon = document.getElementById("volumeIcon")
+
+// Funcion del temporizador del volumen
 function setVolumeIcon() {
   contVolume !== 0
     ? ((volumeIcon.innerHTML = v),
       setTimeout(() => {
         volumeIcon.innerHTML = ""
-      }, VOLUMEN_TEXT_MSG_TIME))
+      }, TEXT_MSG_TIME))
     : (mute(), (toggleMute = true))
 }
+
+// Responde a los botones del volumen
+
 function volume() {
   const volumeButton = document.getElementsByClassName("volume")
 
@@ -107,33 +110,44 @@ function volume() {
 // inicializo bandera volumen y memoria volumen
 let toggleMute = false
 let memoryVolume
+
 // Funcion Mute pone el volumen = 0
 function mute() {
   memoryVolume = contVolume
   contVolume = 0
   volumeIcon.innerHTML = "silencio 🔇 "
 }
+// Funcion boton mute
 const muteButton = document.getElementById("mute")
 muteButton.addEventListener("click", () => {
   toggleMute = !toggleMute
   // pregunto por estado de bandera mute para silenciar o no
   toggleMute
-    ? (mute(), console.log("mute on"))
-    : (console.log("mute off"),
-      //si el volumen era 0, al sacar mute se pone en 1
+    ? mute()
+    : //si el volumen era 0, al sacar mute se pone en 1
       //si el volumen !=0, al sacar volumen vuelve al valor anterior
-      !memoryVolume
+      (!memoryVolume
         ? (contVolume = memoryVolume + 1)
         : (contVolume = memoryVolume),
       (v = ` 🔊 ${contVolume}`),
       setVolumeIcon())
 })
+
+//Inicializo contador de canales
 let channel
 let contChannel = 0
+
+//Responde a los botones para cambiar de canal + y -
 channelButtons()
+
+function channelInfoTimeOut() {
+  setTimeout(() => {
+    document.getElementById("channelInfo").innerHTML = ""
+  }, TEXT_MSG_TIME)
+}
 function channelButtons() {
   const channelButton = document.getElementsByClassName("channel")
-  console.log(channelButton[0])
+
   channelButton[0].addEventListener("click", (evento) => {
     //pregunta si es channel Up o down?
     evento.target.innerHTML === "P +"
@@ -143,15 +157,14 @@ function channelButtons() {
         screen.classList.add(`channel${contChannel}`),
         (document.getElementById(
           "channelInfo"
-        ).innerHTML = `Channel ${contChannel}`))
+        ).innerHTML = `channel ${contChannel}`),
+        channelInfoTimeOut())
       : // Channel Down, decrementa contador y copia a clase channel
         (channel(),
         contChannel--,
         // Pregunta si canal es 0 para q no tenga valores negativos.
         contChannel < 0
-          ? (console.log("es canal 0"),
-            console.log(screen.classList),
-            (contChannel = 9),
+          ? ((contChannel = 9),
             (document.getElementById(
               "channelInfo"
             ).innerHTML = `channel ${contChannel}`),
@@ -159,15 +172,15 @@ function channelButtons() {
           : screen.classList.add(`Channel${contChannel}`),
         (document.getElementById(
           "channelInfo"
-        ).innerHTML = `channel ${contChannel}`))
+        ).innerHTML = `channel ${contChannel}`),
+        channelInfoTimeOut())
 
     function channel() {
-      console.log(screen.classList[0]),
-        // saca el ultimo caracter de screen.classList
-        (contChannel = screen.classList[0].substring(
-          screen.classList[0].length - 1,
-          screen.classList[0].length
-        )),
+      // saca el ultimo caracter de screen.classList
+      ;(contChannel = screen.classList[0].substring(
+        screen.classList[0].length - 1,
+        screen.classList[0].length
+      )),
         (screen.classList = [])
       {
       }
